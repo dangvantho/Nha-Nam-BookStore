9d8e89173f81d91f5888221982e502ba13019894
dangvantho <dangvantho12as0@gmail.com>
2021-03-21 20:35:02 +0700

    deploy to heroku

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,7 @@
 const User=require('../models/User')
 class userController{
     async postUser(req,res){
+        res.header("Access-Control-Allow-Origin", "*")
         const {name,password,email}=req.body
         const data=await User.create({name,password,email})
         console.log(data)
