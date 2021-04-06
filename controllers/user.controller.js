fdc09a39c3fd33c5647426cc4149341d3c13e3b6
dangvantho <dangvantho12as0@gmail.com>
2021-04-06 21:54:13 +0700

    Server: Login

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,11 +1,27 @@
 const User=require('../models/User')
 class userController{
     async postUser(req,res){
-        res.header("Access-Control-Allow-Origin", "*")
         const {name,password,email}=req.body
+        const resCheck=await User.find({name})
+        if(resCheck) {
+            res.json({errs:'Đã tồn tại tài khoản này'})
+            return
+        }
         const data=await User.create({name,password,email})
-        console.log(data)
+        console.log(data,req.body)
         res.json(data)
+    }
+    async postLogin(req,res){
+        const {name,password}= req.body
+        const data= await User.findOne({name,password})
+        if(data) {
+            res.json({...data._doc, isAdmin:data.name==='admin' })
+            return
+        } else {
+            res.json({errs: 'Wrong password or Username'})
+            return
+        }
+
     }
 }
 module.exports= new userController
--- routes/user.route.js
+++ routes/user.route.js
@@ -4,5 +4,6 @@ const router=express.Router()
 
 // [post] /user
 router.post('/user',userController.postUser)
+router.post('/login',userController.postLogin)
 
 module.exports=router
